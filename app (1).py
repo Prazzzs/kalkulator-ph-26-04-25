064f3456f31f8a8dcaaf4a55d93d9d72da1829b7
Prazzzs <adiangga998@gmail.com>
2025-04-26 10:18:11 +0700

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -9,19 +9,7 @@ st.write("You selected:", option)
 st.set_page_config(page_title="Kalkulator pH & pOH", layout="centered")
 
 # Title
-col1, col2, col3 = st.columns(3)
 
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
 # Description
 st.write("""
 Aplikasi ini membantu menghitung nilai pH dan pOH larutan berdasarkan input konsentrasi ion [H⁺] atau [OH⁻].
